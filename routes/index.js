9feb0325ba678731b549d1654fdaf5ce501cc2b2
Leonardo R. Dalpiaz <leonardo.dalpiaz@townsq.com.br>
2026-04-09 18:30:42 -0300

    Set landing page
    Signed-off-by: Leonardo R. Dalpiaz <leonardo.dalpiaz@townsq.com.br>

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/matches/pending');
 });
 
 module.exports = router;
